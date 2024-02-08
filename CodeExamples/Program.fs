a0a6707c4b575500c85b073465e68be0151ea06b
Richard <54506990+RichLarIva@users.noreply.github.com>
2024-02-08 15:59:55 +0100

    First Commit

---
+++ CodeExamples/Program.fs
@@ -0,0 +1,38 @@
+﻿module CodeTests
+
+open System // Gets access to functionality in System namespace.
+
+// Defines a list of names
+let names = ["Reed"; "Jonah"; "Jerome"; "Gina"; "Daisy"; "Gabriella"]
+let numbers = [2; 5; 3; 6; 7; 9]
+
+// Defines a function that takes a name and produces a greeting.
+let getGreeting name = $"Hello, {name}"
+
+let squared number = printfn $"{number} squared is {number * number}"
+
+
+let fib n =
+    let rec loop acc1 acc2 n =
+        match n with
+        | 0 -> acc1
+        | 1 -> acc2
+        | _ ->
+            printfn $"current value: {acc2}"
+            loop acc2 (acc1 + acc2) (n - 1)    
+    loop 0 1 n
+
+let n = 20
+let x = fib(n)
+
+printfn $"fibbonaci({n}): {x}"
+
+// Prints a greeting for each name!
+names
+|> List.map getGreeting
+|> List.iter (fun greeting -> printfn $"{greeting}! Enjoy your F#")
+
+// Prints the squared number of each number
+numbers
+|> List.map squared
+|> List.iter (fun square -> printfn $"{square}")
---
+++ CodeExamplesCS/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HKRCodeExamples
+{
+    class Program
+    {
+
+        static Action<string> greet = name =>
+        {
+            string greeting = $"Hello, {name}";
+            Console.WriteLine($"{greeting}! Enjoy your C#");
+        };
+
+        static Func<int, int> square = x => x * x;
+
+        static Func<int, int> fibonacci = n => n < 2? n:fibonacci(n-1) + fibonacci(n -2);
+
+        static string GreetName(string name)
+        {
+            string greeting = $"Hello, {name}";
+            return $"{greeting}! Enjoy your regular method C#!";
+        }
+
+        static int SquareNumber(int x)
+        {
+            return x * x;
+        }
+
+
+        static void Main(string[] args)
+        {
+            string[] names = { "Reed", "Jonah", "Jerome", "Gina", "Daisy", "Gabriella" };
+            int[] numbers = { 5, 2, 7, 8, 9, 19, 20 };
+            foreach ( string name in names ) 
+            {
+                greet( name );
+                GreetName( name );
+            }
+            foreach(int number in numbers )
+            {
+                Console.WriteLine($"{number} squared is {square(number)}");
+                Console.WriteLine($"{number} squared using method is {SquareNumber(number)}");
+            }
+            for(int i = 0; i <= 20; i++)
+            {
+                Console.WriteLine($"fibonacci of {i} is: {fibonacci(i)}");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
